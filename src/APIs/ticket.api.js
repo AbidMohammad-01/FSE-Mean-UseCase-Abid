bc751bae81231216873c2fbf8a2dd7af2f9fd3d4
AbidMohammad-01 <abid.mohammad2001@gmail.com>
2024-10-03 20:10:31 +0530

    Delete src directory

--- src/APIs/ticket.api.js
+++
@@ -1,8 +0,0 @@
-const exp = require('express')
-const ticketApp = exp.Router()
-const { bookTicket, getBookedTickets } = require('../controllers/ticket.controller')
-const auth = require('../utils/auth')
-ticketApp.get('/gettickets/:id',auth,getBookedTickets)
-ticketApp.post('/bookticket',auth,bookTicket)
-
-module.exports=ticketApp
